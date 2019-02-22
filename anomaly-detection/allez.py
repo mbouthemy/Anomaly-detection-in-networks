a8e250f605c3362b545412ebe1f7dd6a70f822e8
Marin35 <marin.bouthemy@gmail.com>
2019-02-22 15:45:01 +0100

    First commit

---
+++ anomaly-detection/allez.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("ta mere")
+    return(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ anomaly-detection/main.py
@@ -0,0 +1,12 @@
+def main():
+    a = 5
+    b = 4
+    c = addition(a, b)
+    print(c)
+    return(0)
+
+def addition(a, b):
+    return(a + b)
+
+if __name__ == '__main' :
+    main()
