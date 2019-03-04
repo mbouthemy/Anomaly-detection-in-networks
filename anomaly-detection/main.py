fa83205dbfda2656e34ba5b4c3e3a8bcd51b553b
Marin35 <marin.bouthemy@gmail.com>
2019-03-04 15:28:12 +0100

    Get features with Path finder (3.5).

--- anomaly-detection/main.py
+++ anomaly-detection/main.py
@@ -1,51 +1,43 @@
-import numpy as np
-import pandas as pd
-import scipy.stats
-import random
-import math
-import community
 import networkx as nx
-import matplotlib.pyplot as plt
-
-
-
 import anomalies
-import path_finder
-from communities import get_partition, build_community_features
-from localisation import compute_eigen_features
-from utils import upper_eig_generator
+from path_finder import create_features_path_finder
 import utils
 
 
-w = 0.7
-p = 0.05
-number_nodes = 1000
+def main():
+    w = 0.7
+    p = 0.05
+    number_nodes = 50
 
-G = nx.erdos_renyi_graph(number_nodes, p, seed=2, directed=True)
-utils.add_weight(G)
+    G = nx.erdos_renyi_graph(number_nodes, p, seed=2, directed=True)
+    utils.add_weight(G)
+
+    list_of_anomalies = anomalies.selection_of_anomalies()
+    anomalies.info_anomalies(list_of_anomalies)
+    anomalies.insert_anomalies(G, list_of_anomalies, w)
+
+    """
+    # Global features container
+    features = pd.DataFrame()
+    
+    # Compute community feature
+    com_features, HG_parts = build_community_features(G)
+    features = com_features  # Add the
+    
+    
+    loc_feats = pd.DataFrame()
+    for i, part in enumerate(HG_parts):
+        print("Compute {}/{}...".format(i+1, len(HG_parts)))
+        res = compute_eigen_features(part, eig_generator = upper_eig_generator, N_eigs = 20, N_null = 500)
+        loc_feats = loc_feats.append(res)
+    features = features.join(loc_feats)  # Add localisation features
+    """
+
+    # Create the feature based on path finder (3.5)
+    # Real parameters are beam_width = 5000, number_monte_carlo = 500, number to keep (no idea...)
+    features_path_finder = create_features_path_finder(G, beam_width=100, number_monte_carlo=100, number_to_keep=20)
+    print(features_path_finder)
 
 
-list_of_anomalies = anomalies.selection_of_anomalies()
-anomalies.info_anomalies(list_of_anomalies)
-anomalies.insert_anomalies(G, list_of_anomalies, w)
-
-
-# Global features container
-features = pd.DataFrame()
-
-# Compute community feature
-com_features, HG_parts = build_community_features(G)
-features = com_features # Add the 
-
-
-loc_feats = pd.DataFrame()
-for i, part in enumerate(HG_parts):
-    print("Compute {}/{}...".format(i+1, len(HG_parts)))
-    res = compute_eigen_features(part, eig_generator = upper_eig_generator, N_eigs = 20, N_null = 500)
-    loc_feats = loc_feats.append(res)
-features = features.join(loc_feats) # Add localisation features
-
-
-# 3.5 - Path Finder
-paths_to_consider = path_finder.paths(DG, 30)
-print(paths_to_consider)
+if __name__ == '__main__':
+    main()
--- anomaly-detection/path_finder.py
+++ anomaly-detection/path_finder.py
@@ -1,9 +1,112 @@
 # Path finder implementation
 import heapq
+import numpy as np
+import generation
+import utils
+from scipy import special
 from itertools import count
 
 
-def paths(graph, beam_width):
+def create_features_path_finder(graph, beam_width, number_monte_carlo, number_to_keep):
+    """Create features for the path finder (3.5)
+    beam_width = 100
+    number_monte_carlo = 100  # Specify the number of Monte Carlo simulation.
+    number_to_keep = 20  # Specify the number of paths to keep at each iteration of size.
+    """
+    # Get the Monte Carlo statistics (that's a matrix).
+    monte_carlo_stat_path = create_monte_carlo_statistics(graph, number_monte_carlo, beam_width, number_to_keep)
+
+    list_of_path_features = []  # List of the features for each size.
+
+    paths_to_consider = create_paths(graph, beam_width)
+
+    for j in range(19):
+        paths_to_consider = keep_top_path(paths_to_consider, number_to_keep)
+        feature_nodes = calculate_node_statistics(graph, paths_to_consider, monte_carlo_stat_path, j)
+        list_of_path_features.append(feature_nodes)
+        paths_to_consider = increase_path_size(paths_to_consider, graph)
+
+    return list_of_path_features
+
+
+def calculate_node_statistics(graph, path_to_consider, monte_carlo_stat, current_feature):
+    """Compute the p-value for the path finder statistics, and return features"""
+
+    features = np.array([0.] * graph.number_of_nodes())
+
+    for x in path_to_consider:
+        weight, path = x[0], x[2]
+
+        # We compute the p-value with the statistics from Monte Carlo simulation.
+        p_value = utils.p_val_lower(weight, monte_carlo_stat[:, current_feature])
+        # If the p-value is important, we assert a value for every node in that path.
+        if p_value > 0.05:
+            features[path] = special.ndtri(1 - p_value)  # Inverted CDF of gaussian
+
+    return features
+
+
+def create_monte_carlo_statistics(graph, number_monte_carlo, beam_width, number_to_keep):
+    """We store the statistics in a matrix. Each row corresponds to another simulation, and columns to the
+    20 statistics for each path."""
+    number_path = 19
+    monte_carlo_statistics = np.zeros((number_monte_carlo, number_path))
+
+    for T in range(number_monte_carlo):
+
+        graph_null = generation.generate_null(graph)  # Generate the null graph
+        paths = create_paths(graph_null, beam_width)  # Initialise the path
+        paths = keep_top_path(paths, number_to_keep)
+
+        largest_weight = max(paths)[0]  # Get the largest weight.
+        monte_carlo_statistics[T, 0] = largest_weight
+
+        for i in range(1, number_path):  # Because we start with path of size 3
+            paths = increase_path_size(paths, graph)
+            paths = keep_top_path(paths, number_to_keep)
+            largest_weight = max(paths)[0]  # Get the largest weight.
+            monte_carlo_statistics[T, i] = largest_weight
+
+    return monte_carlo_statistics
+
+
+def keep_top_path(paths, number_to_keep):
+    """
+    Keep only the top beam width paths in the heap.
+    """
+    for i in range(len(paths) - number_to_keep):
+        heapq.heappop(paths)
+
+    return paths
+
+
+def increase_path_size(paths, graph):
+    """
+    Increase the size of the paths of 1.
+    """
+    new_list_of_paths = []
+    cnt = count()  # Counter is useful for the heap insertion.
+
+    for x in paths:
+        path = x[2]  # Get the path
+        first_node, last_node = path[0], path[-1]
+
+        for u, v, d in graph.in_edges(first_node, data=True):
+            if u not in path:  # We don't want a loop
+                new_path = [u] + path
+                new_weight = x[0] + d['weight']  # Add the weight to the new
+                heapq.heappush(new_list_of_paths, (new_weight, next(cnt), new_path))
+
+        for u, v, d in graph.out_edges(last_node, data=True):
+            if v not in path:
+                new_path = path + [v]
+                new_weight = x[0] + d['weight']  # Add the weight to the new
+                heapq.heappush(new_list_of_paths, (new_weight, next(cnt), new_path))
+
+    return new_list_of_paths
+
+
+def create_paths(graph, beam_width):
     """
     Create the list of paths of size three to consider.
 
